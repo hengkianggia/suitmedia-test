1e8e72bf24148f79ed27edf185cfa6359cc8c48e
hengkianggia <pengkyjd00@gmail.com>
2023-07-11 13:45:58 +0700

    update service

--- src/Components/OurValues.jsx
+++ src/Components/OurValues.jsx
@@ -4,34 +4,90 @@ import balance from "../assets/balance-scale.png";
 import lightbulb from "../assets/lightbulb-o.png";
 import ValuesItemm from "./ValuesItemm";
 
-const dataItem = [
-  {
-    gambar: lightbulb,
-    title: "INNOVATIVE",
-    kanan: true,
-    kelas: "w-7",
-    warna: "red-400",
-  },
-  {
-    gambar: bank,
-    title: "LOYALTY",
-    kanan: true,
-    warna: "green-400",
-  },
-  {
-    gambar: balance,
-    title: "RESPECT",
-    kanan: false,
-    warna: "blue-400",
-  },
-];
+import { AiOutlineCaretRight } from "react-icons/ai";
+
+// const dataItem = [
+//   {
+//     gambar: lightbulb,
+//     title: "INNOVATIVE",
+//     kanan: true,
+//     kelas: "w-7",
+//     warna: "red-400",
+//   },
+//   {
+//     gambar: bank,
+//     title: "LOYALTY",
+//     kanan: true,
+//     warna: "green-400",
+//   },
+//   {
+//     gambar: balance,
+//     title: "RESPECT",
+//     kanan: false,
+//     warna: "blue-400",
+//   },
+// ];
 
 const OurValues = () => {
   return (
     <div className="flex flex-col items-center w-full py-16">
       <h1 className="text-3xl font-semibold uppercase">our values</h1>
       <div className="flex justify-between items-center w-[75%] mt-5">
-        {dataItem.map((item, index) => (
+        <div className="w-[31.5%] flex items-center relative">
+          <div
+            className={`w-[100%] flex flex-col items-center px-5 py-8 bg-red-400 gap-3 text-white`}
+          >
+            <span>
+              <img src={lightbulb} alt="lightbulb" className={`h-10 w-7`} />
+            </span>
+            <h1 className="text-xl font-semibold">INNOVATIVE</h1>
+            <p className="text-sm text-center">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repudiandae quis natus autem nisi magnam dolorem maxime vitae, eum
+              consectetur quibusdam.
+            </p>
+          </div>
+          <div className={`text-4xl -z-10 text-red-400 absolute -right-6`}>
+            <AiOutlineCaretRight />
+          </div>
+        </div>
+
+        <div className="w-[31.5%] flex items-center relative">
+          <div
+            className={`w-[100%] flex flex-col items-center px-5 py-8 bg-green-400 gap-3 text-white`}
+          >
+            <span>
+              <img src={bank} alt="LOYALTY" className={`h-10 w-7`} />
+            </span>
+            <h1 className="text-xl font-semibold">LOYALTY</h1>
+            <p className="text-sm text-center">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repudiandae quis natus autem nisi magnam dolorem maxime vitae, eum
+              consectetur quibusdam.
+            </p>
+          </div>
+          <div className={`text-4xl -z-10 text-green-400 absolute -right-6`}>
+            <AiOutlineCaretRight />
+          </div>
+        </div>
+
+        <div className="w-[31.5%] flex items-center relative">
+          <div
+            className={`w-[100%] flex flex-col items-center px-5 py-8 bg-blue-400 gap-3 text-white`}
+          >
+            <span>
+              <img src={balance} alt="LOYALTY" className={`h-10 w-7`} />
+            </span>
+            <h1 className="text-xl font-semibold">RESPECT</h1>
+            <p className="text-sm text-center">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Repudiandae quis natus autem nisi magnam dolorem maxime vitae, eum
+              consectetur quibusdam.
+            </p>
+          </div>
+        </div>
+
+        {/* {dataItem.map((item, index) => (
           <ValuesItemm
             key={index}
             title={item.title}
@@ -40,7 +96,7 @@ const OurValues = () => {
             className={item.kelas}
             warna={item.warna}
           />
-        ))}
+        ))} */}
       </div>
     </div>
   );
